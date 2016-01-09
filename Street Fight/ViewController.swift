d26104ea74a240d95c6ff97a925fd58d4ed63a92
Lalit Bagga <Davin12x@Lalits-MacBook-Pro.local>
2016-01-09 15:43:57 -0500

    Ui View Added And Player Initiated

--- Street Fight/ViewController.swift
+++ Street Fight/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var printLabel: UILabel!
+    
+    
+    @IBOutlet weak var playerHpLabel: UILabel!
+    
+    
+    @IBOutlet weak var enemyHpLabel: UILabel!
+    @IBOutlet weak var enemyImage: UIImageView!
+    
+    
+    @IBOutlet weak var chestBtn: UIButton!
+    
+    var player : Player!
+    var enemy : Enemy!
+    
+    
+    @IBAction func onChestTapped(sender: AnyObject) {
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        player = Player(name: "Osama", healthPoint: 110, attackPower: 20)
+        playerHpLabel.text = "\(player.healthPoint)"
     }
 
     override func didReceiveMemoryWarning() {
